91b6bfcb6be9fc186ce540664b0e29d24f72cc25
unknown <nbatre1@yahoo.com>
2019-03-03 23:04:26 -0600

    keep moving forward

--- pixelfinder/src/components/search/Search.js
+++ pixelfinder/src/components/search/Search.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import axios from 'axios';
+import ImageResults from '../image-results/Image-Results';
 
 
  class Search extends Component {
@@ -13,9 +15,23 @@ import MenuItem from '@material-ui/core/MenuItem';
          images:[]
      }
 
+     onTextChange=(e)=>{
+      this.setState({[ e.target.name]:e.target.value},
+        () =>{
+            axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true>`)
+           .then(res =>
+               this.setState({images:res.data.hits}))
+               .catch(err => console.log(err));
+     })
+    }
 
- 
+
+    onAmountChange =(e,index,value)=>{
+        this.setState({amount:e.target.value})
+         }
+
     render(){
+        console.log(this.state.images)
         return(
             <div>
            <TextField 
@@ -28,14 +44,23 @@ import MenuItem from '@material-ui/core/MenuItem';
             <br />
             <Select
             name="amount"
-            value={this.state.amount}>
+            value={this.state.amount}
+            onChange={this.onAmountChange}>
     <MenuItem value={0}>
       <em>None</em>
     </MenuItem>
-    <MenuItem value={10}>Ten</MenuItem>
-    <MenuItem value={20}>Twenty</MenuItem>
-    <MenuItem value={30}>Thirty</MenuItem>
+    <MenuItem value={5}>5</MenuItem>
+    <MenuItem value={10}>10</MenuItem>
+    <MenuItem value={15}>15</MenuItem>
+    <MenuItem value={30}>30</MenuItem>
+    <MenuItem value={50}>50</MenuItem>
   </Select>
+  <br />
+
+  {
+      this.state.images.length > 0 ? (<ImageResults images={this.state.images} />):
+      null
+  }
             </div>
 
         )
