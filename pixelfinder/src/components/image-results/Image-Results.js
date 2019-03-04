8da06c7a42589f5358cb60507880b3cb7eea08b6
unknown <nbatre1@yahoo.com>
2019-03-04 06:55:18 -0600

    keep moving forward

--- pixelfinder/src/components/image-results/Image-Results.js
+++ pixelfinder/src/components/image-results/Image-Results.js
@@ -1,15 +1,49 @@
 import React,{Component} from 'react';
 import PropTypes from 'prop-types';
-import ZoomIn from '@material-ui/core/Zoom';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import Dialog from '@material-ui/core/Dialog';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
-import ListSubheader from '@material-ui/core/ListSubheader';
-import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import Icon from '@material-ui/core/Icon';
+import DeleteIcon from '@material-ui/icons/Delete';
+import NavigationIcon from '@material-ui/icons/Navigation';
+
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    color:'white'
+  },
+  icon: {
+    margin: theme.spacing.unit * 2,
+  },
+ 
+});
 
 
 class ImageResults extends Component {
+  
+  state ={
+    open:false,
+    currentImg: ''
+  }
+
+  handleOpen=(img)=>{
+  this.setState({
+    open:true,
+    currentImg:img
+  })
+  }
+
+  handleClose=()=>{
+    this.setState({open:false});
+  }
+
+
 
     render(){
         let imageListContent;
@@ -25,9 +59,14 @@ class ImageResults extends Component {
                       title={img.tags}
                       subtitle={<span>by: {img.user}</span>}
                       actionIcon={
-                        <IconButton >
-                          
-                        </IconButton>
+                     
+                        <Fab color="primary">
+                            <NavigationIcon color="secondary" onClick={()=> this.handleOpen(img.largeImageURL)} className={styles.icon} color="error">
+                            Click
+                         </NavigationIcon>
+
+                        </Fab>
+                   
                       }
                     />
 
@@ -42,10 +81,29 @@ class ImageResults extends Component {
           }
         
        
-
+       const actions = [
+         <Fab label ="Close" 
+         disabled aria-label="Delete"
+         className={styles.icon}
+         primary={true} 
+         onClick={this.handleClose}>
+         <DeleteIcon />
+         </Fab>
+       ]
         return(
             <div>
                 {imageListContent}
+                <Dialog
+                actions ={actions}
+                modal={false}
+                open={this.state.open}
+                onRequestClose={this.handleClose}
+                >
+                <img src={this.state.currentImg}
+                 alt=""
+                 style={{width:'100%'}} />
+
+                </Dialog>
             </div>
         )
     }
